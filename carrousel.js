40e5ac34e5675572817ded09d1c05dacb2d49267
didierguy <did17486@hotmail.fr>
2022-10-06 12:25:47 +0200

    ajout rome(html et css),carrousel(css et js) et /images

--- carrousel.js
+++ carrousel.js
@@ -1,39 +1,46 @@
-document.body.onload = function () {
-    nbr = 5;
-    p = 0;
-    container = document.getElementById("container");
-    g = document.getElementById("g");
-    d = document.getElementById("d");
-    container.style.width = (800 * nbr) + "px";
-    for (i = 1; i <= nbr; i++) {
-        div = document.createElement("div");
-        div.className = "photo";
-        div.style.backgroundImage = 'url("images/im' + i + '.jpg")';
-        container.appendChild(div);
+let img_slider = document.getElementsByClassName('img_slider');
+
+let etape = 0;
+
+let nbr_img = img_slider.length;
+
+let precedent = document.querySelector('.precedent');
+let suivant = document.querySelector('.suivant');
+
+function enleverActiveImages() {
+    for (let i = 0; i < nbr_img; i++) {
+        img_slider[i].classList.remove('active');
     }
-    afficherMasquer();
 }
-g.onclick = function () {
-    if (p > -nbr + 1)
-        p--;
-    container.style.transform = "translate(" + p * 800 + "px) ";
-    container.style.transistion = "all 0.5s ease";
-    afficherMasquer();
+
+//Action right button
+suivant.addEventListener('click', function () {
+    etape++;
+    if (etape >= nbr_img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img_slider[etape].classList.add('active');
 }
-d.onclick = function () {
-    if (p < 0)
-        p++;
-    container.style.transform = "translate(" + p * 800 + "px)";
-    container.style.transistion = "all 0.5s ease";
-    afficherMasquer();
+)
+
+//Action left button
+precedent.addEventListener('click', function () {
+    etape--;
+    if (etape < 0) {
+        etape = nbr_img - 1;
+    }
+    enleverActiveImages();
+    img_slider[etape].classList.add('active');
 }
-function afficherMasquer() {
-    if (p == -nbr + 1)
-        g.style.visibility = "hidden";
-    else
-        g.style.visibility = "visible";
-    if (p == 0)
-        d.style.visibility = "hidden";
-    else
-        d.style.visibility = "visible";
-}
+)
+
+//Timer carrousel
+setInterval(function () {
+    etape++;
+    if (etape >= nbr_img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img_slider[etape].classList.add('active');
+}, 5000)
